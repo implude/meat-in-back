78cb5bb47353c1d627ba0a4c689457d7d78f66c8
RyCont(정한) <35295182+rycont@users.noreply.github.com>
2021-10-14 11:24:47 +0000

    feat: implement createPost endpoint

--- api/post.ts
+++ api/post.ts
@@ -1,11 +1,58 @@
+import { gql } from "@keystone-next/keystone";
 import { KeystoneContext } from "@keystone-next/keystone/types";
 import type { Request, Response } from 'express';
+
+const WHOLE_POST_QUERY = gql`
+photo,
+title,
+created_at
+author {
+    name,
+    photo,
+    rep_badge {
+        image,
+        label,
+        description
+    }
+}
+content,
+linked_recipe {
+    name,
+    thumbnail,
+    meat_type {
+        label
+    }
+    duration
+    difficulty {
+        label
+    }
+    created_at
+    hearted_user {
+        id
+    }
+}
+comment {
+    author {
+        name,
+        photo,
+        id
+    }
+    created_at
+    content
+}
+hearted_user {
+    id
+}
+bookmarked_user {
+    id
+}
+`
 
 export const getCuratedPost = async (req: Request, res: Response) => {
     const context = (req as any).context as KeystoneContext;
     // context.session
     const posts = await context.query.Post.findMany({
-        query: `
+        query: gql`
             author {
                 name,
                 photo,
@@ -49,7 +96,6 @@ export const getCuratedPost = async (req: Request, res: Response) => {
 
 export const getSpecificPost = async (req: Request, res: Response) => {
     const context = (req as any).context as KeystoneContext;
-    console.log(req.params.id)
     if (typeof req.params.id !== 'string') {
         res
             .status(400)
@@ -64,54 +110,20 @@ export const getSpecificPost = async (req: Request, res: Response) => {
         where: {
             id: req.params.id
         },
-        query: `
-            photo,
-            title,
-            created_at,
-            author {
-                name,
-                photo,
-                rep_badge {
-                    image,
-                    label,
-                    description
-                }
-            }
-            content,
-            linked_recipe {
-                name,
-                thumbnail,
-                meat_type {
-                    label
-                }
-                duration
-                difficulty {
-                    label
-                }
-                created_at
-                hearted_user {
-                    id
-                }
-            }
-            comment {
-                author {
-                    name,
-                    photo,
-                    id
-                }
-                created_at
-                content
-            }
-            hearted_user {
-                id
-            }
-            bookmarked_user {
-                id
-            }
-        `
+        query: WHOLE_POST_QUERY
+    })
+    if (queried) res.json(queried)
+}
+
+export const createPost = async (req: Request, res: Response) => {
+    const context = (req as any).context as KeystoneContext;
+    console.log(req.query)
+
+    const createdResult = await context.query.Post.createOne({
+        // TODO: IMPLEMENT AUTHOR, EXAMPLE: NUTYWORKS
+        data: { ...req.query, author: "ckuphca1e0164tl9o8xl3iqvs" },
+        query: WHOLE_POST_QUERY
     })
 
-    // IMMEDIETELY TODO: PROCESS RESULT!
-    console.log(queried)
-    if (queried) res.json(queried)
+    if (createdResult.id) res.json(createdResult)
 }
